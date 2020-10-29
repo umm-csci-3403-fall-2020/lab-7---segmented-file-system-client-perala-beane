6916c2fbc3148f2caddf5a7661676cbdac61a631
Alarep <peralaj94@gmail.com>
2020-10-29 10:55:06 -0500

    Create the DataPacket class and write the logic.

--- src/segmentedfilesystem/DataPacket.java
+++ src/segmentedfilesystem/DataPacket.java
@@ -11,12 +11,35 @@ public class DataPacket {
     int packetNumber;
     byte fileID;
     boolean wasAdded;
+    byte[] copy;
+    int length; //length of array
 
     public DataPacket(DatagramPacket packet) {
+        this.data = packet.getData();
+        this.setPacketNumber();
+        this.fileID = data[1];
+        this.status = data[0];
+        length = packet.getLength();
+        this.copy = Arrays.copyOfRange(packet.getData(), 4, length); //skips the first 4 because those are for bookkeeping.
+        this.wasAdded = false;
 
     }
 
-    public void setPacketNumber() {
-        
+    public void setPacketNumber() { //from documentation
+        int x = data[2];
+        int y = data[3];
+
+        if (x < 0) {
+            x += 256;
+        }
+        if (y < 0) {
+            x += 256;
+        }
+
+        this.packetNumber = (256 * x + y);
+    }
+
+    public byte returnFileID() {
+        return fileID;
     }
 }
