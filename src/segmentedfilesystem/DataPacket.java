5716614816e3d842423eb94dc442d2217310cb32
Alarep <peralaj94@gmail.com>
2020-10-29 10:57:42 -0500

    Finish some final touches in DataPacket and PacketManager.

--- src/segmentedfilesystem/DataPacket.java
+++ src/segmentedfilesystem/DataPacket.java
@@ -3,8 +3,6 @@ package segmentedfilesystem;
 import java.net.DatagramPacket;
 import java.util.Arrays;
 
-//the status bytes, file ID, packet number, and data, and store them in 
-//fields that are accessible through various get methods
 public class DataPacket {
     byte status;
     byte[] data;
@@ -17,8 +15,8 @@ public class DataPacket {
     public DataPacket(DatagramPacket packet) {
         this.data = packet.getData();
         this.setPacketNumber();
-        this.fileID = data[1];
-        this.status = data[0];
+        this.fileID = data[1]; //from slideshow
+        this.status = data[0]; //from slideshow
         length = packet.getLength();
         this.copy = Arrays.copyOfRange(packet.getData(), 4, length); //skips the first 4 because those are for bookkeeping.
         this.wasAdded = false;
@@ -26,8 +24,8 @@ public class DataPacket {
     }
 
     public void setPacketNumber() { //from documentation
-        int x = data[2];
-        int y = data[3];
+        int x = data[2]; //from slideshow
+        int y = data[3]; //from slideshow
 
         if (x < 0) {
             x += 256;
--- src/segmentedfilesystem/PacketManager.java
+++ src/segmentedfilesystem/PacketManager.java
@@ -1,7 +1,33 @@
 package segmentedfilesystem;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class PacketManager {
-    
+    int maxSize;
+    byte fileID;
+    List<DataPacket> data;
+    boolean isFull;
+
+    public PacketManager(byte fileID) {
+        this.data = new ArrayList<DataPacket>();
+        this.isFull = false;
+        this.maxSize = 102404; //1024 * 99 + 1028
+        this.fileID = fileID;
+    }
+
+    public void listAdd(DataPacket dataPacket) {
+        data.add(dataPacket);
+        if (data.size() == this.maxSize) {
+            this.isFull = true;
+        }
+    }
+
+    public void setFileID(byte fileID) {
+        this.fileID = fileID;
+    }
+
+    public void maxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }    
 }
