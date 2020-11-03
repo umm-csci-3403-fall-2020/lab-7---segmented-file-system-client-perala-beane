0d415604b216fcb5978dd73e9feb2349820f96e7
Alarep <peralaj94@gmail.com>
2020-11-03 11:23:52 -0600

    Moved the test files to the appropriate directory.

---
+++ test/segmentedfilesystem/HeaderTests.java
@@ -0,0 +1,28 @@
+package segmentedfilesystem;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class HeaderTests {
+    
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void testFilename() {
+
+    }
+
+    @Test 
+    public void testFileID() {
+
+    }
+
+    @Test
+    public void testFilenameLength() {
+
+    }
+}
